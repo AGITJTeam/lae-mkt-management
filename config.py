36022275b74cacc7ff7b711eaeb29a898b61549b
cyberspacehead <luismi@agibusiness.com>
2025-02-07 15:32:48 -0800

    FIX: token expiration time

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
+from datetime import timedelta
 import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "default")
-    JWT_ACCESS_TOKEN_EXPIRES = int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES", 9))
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES", 9)))
 
     @staticmethod
     def get_username():
